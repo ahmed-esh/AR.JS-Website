4fa58fbe9b928a76151a7620bee5cb6d25e164b9
ahmed-esh <ahmedshuwehdi@bennington.edu>
2024-12-26 00:22:57 -0500

    Add files via upload

---
+++ JS/app.js
@@ -0,0 +1,5 @@
+document.getElementById('start-button').addEventListener('click', () => {
+    document.getElementById('start-screen').style.display = 'none';
+    document.querySelector('a-scene').style.display = 'block';
+  });
+  
